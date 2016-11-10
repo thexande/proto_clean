4706f31db7bc9fa2d98bc806589d644f215cb371
Alexander Murphy <amurphy345@gmail.com>
2016-11-10 16:31:58 -0700

    adding helpers and protocols

---
+++ proto/Protocols/ModalViewControllerDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  ModalViewControllerDelegate.swift
+//  proto
+//
+//  Created by Alexander Murphy on 11/1/16.
+//  Copyright © 2016 Alexander Murphy. All rights reserved.
+//
+import Foundation
